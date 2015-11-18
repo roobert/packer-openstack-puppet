1e3ab4b683e50aa57e3c165ee6820e01fd0ebcf7
Rob Wilson <roobert@gmail.com>
2015-11-18 16:44:18 +0000

    disable management of hosts file

--- assets/site.pp
+++ assets/site.pp
@@ -5,3 +5,7 @@ file { '/etc/hostname':
 file { '/etc/hosts':
   content => template('/tmp/puppet/hosts.erb')
 }
+
+exec { 'cloud_init_manage_etc_hosts_false':
+  command => "sed -i 's/manage_etc_hosts: true/manage_etc_hosts: false/' /etc/cloud/cloud.cfg"
+}
